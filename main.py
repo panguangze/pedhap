c9ef4ca5c364c9c1434ff3e43fc68d6d9c16de32
panguangze <guangzep@gmail.com>
2021-09-02 19:57:07 +0800

    init

--- main.py
+++ main.py
@@ -86,10 +86,11 @@ def main():
     for f in families:
         all_trios = ped_utils.get_trios(f)
         for chromo in phaser.chromos:
+            pass
             # up_to_down(all_trios, phaser, chromo)
             # down_to_up(all_trios, phaser, chromo)
             # while phaser.check_phasing_state(chromo):
-            up_to_down(all_trios, phaser, chromo)
+            # up_to_down(all_trios, phaser, chromo)
             # down_to_up(all_trios, phaser, chromo)
     phaser.write()
     # phaser.write_simple("s0210-1_FDHG190451805-1a")
